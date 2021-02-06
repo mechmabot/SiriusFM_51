177b8e60c5f954a39f6eaabf3feb445051bb31b5
mechmabot <ukhin.ilya.alex@yandex.ru>
2021-02-06 09:55:49 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "DiffusionCEV.h"
+#include "DiffusionOU.h"
+#include "DiffusionLipton.h"
+#include "DiffusionCIR.h"
+#include "DiffusionGBM.h"
+int main(void){
+  std::cout << "Ok\n";
+  return 0;
+}
